83de5e71785bd6922cd0ada585931fef986436e2
Attlow <vinicius.alexander2004@gmail.com>
2024-05-29 21:40:07 -0300

    Registro

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -53,7 +53,7 @@ namespace MvcMovie.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Nome,cpf")] Usuario usuario)
+        public async Task<IActionResult> Create([Bind("Id,Nome,cpf,Email,Senha")] Usuario usuario)
         {
             if (ModelState.IsValid)
             {
@@ -85,7 +85,7 @@ namespace MvcMovie.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,cpf")] Usuario usuario)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,cpf,Email,Senha")] Usuario usuario)
         {
             if (id != usuario.Id)
             {
--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -10,14 +10,29 @@ namespace MvcMovie.Models
         [Display(Name = "Código")]
         public int Id { get; set; }
 
+        [Required]
         [Column("Nome")]
         [Display(Name = "Nome")]
         public string Nome { get; set; }
 
+        [Required]
         [Column("CPF")]
         [Display(Name = "CPF")]
         [MinLength(11, ErrorMessage = "O campo deve ter no mínimo 11 caracteres.")]
         [RegularExpression(@"^\d+$", ErrorMessage = "O campo deve conter apenas números.")]
         public string cpf { get; set; }
+
+        [Required(ErrorMessage = "Insira seu E-mail.")]
+        [Column("Email")]
+        [Display(Name = "Email")]
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail inválido.")]
+        public string email { get; set; }
+
+        [Column("Senha")]
+        [Required(ErrorMessage = "Insira sua senha")]
+        [Display(Name = "Senha")]
+        [StringLength(20, ErrorMessage = "Senha deve ter pelo menos 8 caracteres.", MinimumLength = 8)]
+        public string Password { get; set; }
+
     }
 }
